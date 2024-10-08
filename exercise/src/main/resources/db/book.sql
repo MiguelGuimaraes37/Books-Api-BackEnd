a8e97575d2552ab56d454ddff5b05c5fa4deabdd
deli <deli@gmail.com>
2024-10-09 00:11:10 +0100

    Fixed some errors

--- exercise/src/main/resources/db/book.sql
+++ exercise/src/main/resources/db/book.sql
@@ -1,10 +1,10 @@
 DELETE FROM book;
 
 INSERT INTO book (title, author, isbn, publishedDate, price)
-VALUES ('Book 1', 'Author 1', '978-3-16-148410-0', '2023-01-01', 300);
+VALUES ('Book 1', 'Author 1', '978-3-16-148410-0', '23-01-1899', 300);
 
 INSERT INTO book (title, author, isbn, publishedDate, price)
-VALUES ('Book 2', 'Author 2', '978-0-262-13472-9', '2023-05-15', 500);
+VALUES ('Book 2', 'Author 2', '978-0-262-13472-9', '20-05-1500', 500);
 
 INSERT INTO book (title, author, isbn, publishedDate, price)
-VALUES ('Book 4', 'Author 3', '978-1-56619-909-4', '2024-03-20', 600);
+VALUES ('Book 4', 'Author 3', '978-1-56619-909-4', '24-03-2023', 600);
